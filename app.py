3af1dfe08708090bc0aaff8e190cf1d6bf152c2b
jebinshaju <jebinshaju4@gmail.com>
2023-11-17 00:34:52 +0530

    Updated api

--- app.py
+++ app.py
@@ -1,57 +1,9 @@
-from flask import Flask, jsonify
-import random
-from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
-
-exoplanet_data = []
-galaxy_data = []
-
-def fetch_and_shuffle_exoplanet_data():
-    table = NasaExoplanetArchive.query_criteria(table="PS", select="pl_name")
-    exoplanet_data.extend([row['pl_name'] for row in table])
-    random.shuffle(exoplanet_data)
-
-def fetch_and_shuffle_galaxy_data():
-    url = "https://en.wikipedia.org/wiki/List_of_galaxies"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find("table", class_="wikitable")
-        galaxy_data.clear()
-
-        for row in table.find_all('tr')[1:]:
-            columns = row.find_all('td')
-            if len(columns) >= 5:
-                image_column = columns[0].find("img")
-                original_url = image_column["src"] if image_column else "No image available"
-                
-                if "70px-" in original_url:
-                    original_url = original_url.replace("70px-", "")
-                parts = original_url.split("/")
-                file_name = parts[-1]
-                image_url = f"https://en.wikipedia.org/wiki/List_of_galaxies#/media/File:{file_name}"
-
-                galaxy = columns[1].find("a").text.strip()
-                constellation = columns[2].find("a").text.strip()
-                origin_name = columns[3].text.strip()
-                note = columns[4].text.strip()
-
-                galaxy_data.append({
-                    'Galaxy': galaxy,
-                    'Constellation': constellation,
-                    'Origin of Name': origin_name,
-                    'Notes': note,
-                    'Image URL': image_url
-                })
-
-    random.shuffle(galaxy_data)
-
-fetch_and_shuffle_exoplanet_data()
-fetch_and_shuffle_galaxy_data()
 
 def extract_exoplanet_details(exoplanet_name):
     exoplanet_name_with_underscore = exoplanet_name.replace(" ", "_")
@@ -68,6 +20,8 @@ def extract_exoplanet_details(exoplanet_name):
             description_element = soup.find("b", text=exoplanet_name)
             if description_element:
                 description = description_element.find_next("li").get_text().strip()
+                # Remove newline characters
+                description = description.replace("\n", "")
 
             img_element = soup.find("img", src=True)
             if img_element:
@@ -85,22 +39,97 @@ def extract_exoplanet_details(exoplanet_name):
 
     return exoplanet_info
 
-@app.route('/exoplanets', methods=['GET'])
-def get_random_exoplanet():
-    if not exoplanet_data:
-        return jsonify({'error': 'Exoplanet data is empty'})
-    
-    exoplanet_name = exoplanet_data.pop()
-    exoplanet_info = extract_exoplanet_details(exoplanet_name)
+def get_galaxy_details(galaxy_name):
+    url = "https://images-api.nasa.gov/search"
+    params = {
+        "q": galaxy_name,
+        "media_type": "image",
+    }
 
-    return jsonify(exoplanet_info)
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    galaxies = []
+    for item in data["collection"]["items"]:
+        galaxy = {
+            "title": item["data"][0]["title"],
+            "description": item["data"][0]["description"],
+            "image_url": item["links"][0]["href"]
+        }
+        galaxies.append(galaxy)
+
+    # Return the first galaxy that matches the name
+    for galaxy in galaxies:
+        if galaxy["title"].lower() == galaxy_name.lower():
+            return galaxy
+
+    # If no galaxy matches the name, return an error message
+    return {'error': 'No galaxy found with the given name.'}
+
+@app.route('/exoplanets', methods=['GET'])
+def get_exoplanets():
+    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+top+300+*+from+ps+where+tran_flag=1+order+by+pl_name+asc"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    rows = soup.find_all('tr')
+    exoplanets = []
+    for row in rows[1:]:  # Skip the header row
+        columns = row.find_all('td')
+        exoplanet = {
+            "planet_name": columns[0].text,
+            "star_name": columns[2].text,
+            "discovery_method": columns[6].text,
+            "orbital_period": columns[7].text,
+            "planet_mass": columns[22].text,
+            "planet_radius": columns[23].text,
+            "equilibrium_temperature": columns[24].text
+        }
+        exoplanet_details = extract_exoplanet_details(exoplanet["planet_name"])
+        exoplanet["description"] = exoplanet_details["description"]
+        exoplanet["image_url"] = exoplanet_details["image_url"]
+        exoplanets.append(exoplanet)
+
+    # Remove duplicate exoplanets
+    exoplanets = [exoplanet for i, exoplanet in enumerate(exoplanets) if exoplanets.index(exoplanet) == i]
+
+    return jsonify(exoplanets)
 
 @app.route('/galaxies', methods=['GET'])
-def get_random_galaxy():
-    if not galaxy_data:
-        fetch_and_shuffle_galaxy_data()
-    random_galaxy = random.choice(galaxy_data)
-    return jsonify(random_galaxy)
+def get_galaxies():
+    url = "https://images-api.nasa.gov/search"
+    params = {
+        "q": "galaxy",
+        "media_type": "image",
+    }
 
-if __name__ == '__main__':
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    galaxies = []
+    for item in data["collection"]["items"]:
+        desc = item["data"][0]["description"]
+        desc = desc.replace("&amp; NASA  <b><a href=\"http://www.nasa.gov/audience/formedia/features/MP_Photo_Guidelines.html\" rel=\"nofollow\">NASA image use policy.</a></b>  <b><a href=\"http://www.nasa.gov/centers/goddard/home/index.html\" rel=\"nofollow\">NASA Goddard Space Flight Center</a></b> enables NASA’s mission through four scientific endeavors: Earth Science, Heliophysics, Solar System Exploration, and Astrophysics. Goddard plays a leading role in NASA’s accomplishments by contributing compelling scientific knowledge to advance the Agency’s mission.  <b>Follow us on <a href=\"http://twitter.com/NASAGoddardPix\" rel=\"nofollow\">Twitter</a></b>  <b>Like us on <a href=\"http://www.facebook.com/pages/Greenbelt-MD/NASA-Goddard/395013845897?ref=tsd\" rel=\"nofollow\">Facebook</a></b>  <b>Find us on <a href=\"http://instagrid.me/nasagoddard/?vm=grid\" rel=\"nofollow\">Instagram</a></b> ","")
+        galaxy = {
+            "title": item["data"][0]["title"],
+            "description": desc,
+            "image_url": item["links"][0]["href"]
+        }
+        galaxies.append(galaxy)
+
+    return jsonify(galaxies)
+
+@app.route('/', methods=['GET'])
+def get_random_detail():
+    detail_type = random.choice(['exoplanet', 'galaxy'])
+
+    if detail_type == 'exoplanet':
+        exoplanet_name = random.choice(get_exoplanets().json)["planet_name"]
+        return jsonify(extract_exoplanet_details(exoplanet_name))
+    elif detail_type == 'galaxy':
+        galaxy_name = random.choice(get_galaxies().json)["title"]
+        return jsonify({"galaxy": get_galaxy_details(galaxy_name)})
+
+if __name__ == "__main__":
     app.run(debug=True)
